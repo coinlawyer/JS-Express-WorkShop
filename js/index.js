eb1617ceeb2efecec6b779ae9b3aaa9e02ffb8da
Andrii Chyryk <achyryk86@gmail.com>
2020-01-17 23:59:31 +0200

    add parameters to the createCart()

--- js/index.js
+++ js/index.js
@@ -7,20 +7,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const logoHeader = document.querySelector('.logo');
     const goodsWrapper = document.querySelector('.goods-wrapper');
 
-    const createCart = () => {
+    const createCart = (id, title, price, img) => {
         const cart = document.createElement('div');
         cart.className = 'card-wrapper col-12 col-md-6 col-lg-4 col-xl-3 pb-3';
         cart.innerHTML = `
             <div class="card">
                 <div class="card-img-wrapper">
-                    <img class="card-img-top" src="img/temp.png" alt="">
-                    <button class="card-add-wishlist"></button>
+                    <img class="card-img-top" src="${img}" alt="">
+                    <button class="card-add-wishlist"
+                    data-item-id="${id}"></button>
                 </div>
             <div class="card-body justify-content-between">
-                <a href="#" class="card-title">Item name</a>
-                <div class="card-price">100 $</div>
+                <a href="#" class="card-title">${title}</a>
+                <div class="card-price">${price}$</div>
                     <div>
-                        <button class="card-add-cart">Add to cart</button>
+                        <button class="card-add-cart"
+                        data-item-id="${id}">Add to cart</button>
                     </div>
                 </div>
             </div>`;
